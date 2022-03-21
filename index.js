328a3ff5f7650c85071f0403e50f1797e60de6e8
Throsby Wells <throsbywells@gmail.com>
2022-03-21 17:18:04 -0400

    commit tree 62235fb6ea00808f9379acc347bdc5c65339d4e1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function superbowlWin(record){
+    const wins = record.find((el) => {if (el.result === "W") {
+        return el.year;
+    };
+    });
+    // console.log([wins])
+    if (wins != undefined){
+        return String(wins.year)
+    }
+    else{
+        return undefined
+    }
+}
+
